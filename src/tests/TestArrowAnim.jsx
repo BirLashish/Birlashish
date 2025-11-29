2edf97ea12b5c536bb504937a640d5537589cd0d
forger6969 <saidazim186@gmail.com>
2025-11-29 13:51:33 +0500

    ad

--- src/tests/TestArrowAnim.jsx
+++ src/tests/TestArrowAnim.jsx
@@ -1,20 +1,9 @@
-import React from "react";
-import { motion, useTransform, useScroll } from "motion/react";
+import React from 'react'
 
 const TestArrowAnim = () => {
-  const { scrollYProgress } = useScroll();
-
-  // Преобразуем 0-1 в 0-1 (scaleX)
-  const scaleX = useTransform(scrollYProgress, [0, 1], [0, 1]);
-
   return (
-    <>
-      <motion.div
-        style={{ scaleX }}
-        className="fixed top-0 left-0 h-2 w-full bg-blue-500 origin-left"
-      />
-    </>
-  );
-};
+    <div>TestArrowAnim</div>
+  )
+}
 
-export default TestArrowAnim;
+export default TestArrowAnim
